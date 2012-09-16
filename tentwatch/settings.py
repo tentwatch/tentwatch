371c0844fda482bcf6f650c4cc5f59c32ed5c626
Micah Hausler <hackanooga@ubuntu.(none)>
2012-09-15 19:50:00 -0500

    implemented bcrypt

--- tentwatch/settings.py
+++ tentwatch/settings.py
@@ -127,6 +127,16 @@ INSTALLED_APPS = (
     'tentwatch.import'
 )
 
+# Default Hasher first, accepted hashes follow
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
